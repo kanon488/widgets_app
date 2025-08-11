840b7c7014130ab421ff3ab251d29e753a47528d
Eduardo Ortiz Espinoza <kanon488@hotmail.com>
2025-08-10 21:11:55 -0600

    Screen created and exported in common file

--- lib/config/presentation/screens/screens.dart
+++ lib/config/presentation/screens/screens.dart
@@ -1,4 +1,12 @@
+
+export 'package:widgets_app/config/presentation/screens/animated/animated_screen.dart';
+export 'package:widgets_app/config/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widgets_app/config/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/config/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets_app/config/presentation/screens/home/home_screen.dart';
+export 'package:widgets_app/config/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets_app/config/presentation/screens/progress/progress_screen.dart';
+export 'package:widgets_app/config/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_app/config/presentation/screens/ui_controls/ui_controls_screen.dart';
 
+ 
